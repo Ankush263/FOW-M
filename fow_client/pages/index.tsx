e937ee493a663ff7db710e1b6ee84f27bfbd6cb8
Ankush263 <ankushbanik263@gmail.com>
2023-03-21 11:09:53 +0530

    update

--- fow_client/pages/index.tsx
+++ fow_client/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import FirstPage from './components/CELO-HACK/FirstPage';
+import FirstPage from './components/Project/FirstPage';
 
 const Home: NextPage = () => {
   return (
